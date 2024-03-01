bce754bb67a2a6d87efd1e7cb8269dd3e9b0a34b
EC2 Default User <ec2-user@ip-172-31-9-130.eu-north-1.compute.internal>
2024-03-01 16:19:58 +0000

    Including label to inputs and a submit button

--- food-order-form/src/FoodOrderForm.js
+++ food-order-form/src/FoodOrderForm.js
@@ -4,10 +4,15 @@ function FoodOrderForm() {
 
 return (
   <form>
+    <label htmlFor="name">Name</label>
     <input id="name" />
+    <label htmlFor="phone" >Phone</label>
     <input id="phone" />
+    <label htmlFor="address" >Address</label>
     <input id="address" />
+    <label htmlFor="order" >Order</label>
     <input id="order" />
+    <button type="submit" >Submit Order</button>
   </form>
 )
 }
