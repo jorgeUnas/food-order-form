0032c5e7658b8ed9fd36594aa009786f96703423
EC2 Default User <ec2-user@ip-172-31-9-130.eu-north-1.compute.internal>
2024-03-04 08:37:12 +0000

    adding hooks and values to the inputs

--- food-order-form/src/FoodOrderForm.js
+++ food-order-form/src/FoodOrderForm.js
@@ -1,17 +1,20 @@
 import React, { useState } from "react";
 
 function FoodOrderForm() {
-
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [order, setOrder] = useState('');
 return (
   <form>
     <label htmlFor="name">Name</label>
-    <input id="name" />
+    <input id="name" valeu={name} />
     <label htmlFor="phone" >Phone</label>
-    <input id="phone" />
+    <input id="phone" value={phone} />
     <label htmlFor="address" >Address</label>
-    <input id="address" />
+    <input id="address" value={address} />
     <label htmlFor="order" >Order</label>
-    <input id="order" />
+    <input id="order" value={order} />
     <button type="submit" >Submit Order</button>
   </form>
 )
